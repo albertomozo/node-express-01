28b08397661ce1f65ad51a23971171910397159e
albertomozodev <albertomozodesarrollador@gmail.com>
2023-04-03 09:51:17 +0200

    templates instacion pug primera vista nosotros.pug

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 import express from 'express';
 import router from './routes/index.js';
-// impporto las rutas del fichero route.js
+// importo las rutas del fichero route.js
 
 const app = express();
 // definir puerto
 const port = process.env.PORT ||4000;
 // Agregar riouter 
 app.use('/',router);
+app.set('view engine','pug');
 
 
 app.listen(port, () => {
