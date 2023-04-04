53560fadc84e2369c0cf11420309c3ae5b5f1071
albertomozodev <albertomozodesarrollador@gmail.com>
2023-04-04 12:52:55 +0200

    App Local variables

--- index.js
+++ index.js
@@ -11,7 +11,11 @@ app.use('/',router);
 app.set('view engine','pug');
 // agregamos uso de ficheros estáticos
 app.use(express.static('public'))
+// vamos a obtener el dia actual para colocar en el pie
 
+const fecha = new Date;
+app.locals.actualFecha = `${fecha.getHours()} : ${fecha.getMinutes()} : ${fecha.getSeconds()}  `;
+app.locals.miValor = 'Proyectos a Medida ';
 
 app.listen(port, () => {
     console.log(`El servidor está funcionando en el puerto ${port}`);
